00601608629aed4ab870620a782535e4cee1fcc9
Diez Roggisch <deets@web.de>
2026-09-13 15:09:07 +0200

    Wifi AP `silvano-bot` setup

--- silvano-bot-firmware-idf/src/main.rs
+++ silvano-bot-firmware-idf/src/main.rs
@@ -1,14 +1,21 @@
 use display::SilvanoBotDisplay;
-use esp_idf_svc::hal::{
-    delay::{FreeRtos, BLOCK},
-    i2c::{I2cConfig, I2cDriver},
-    peripherals::Peripherals,
-    units::*,
+use esp_idf_svc::{
+    eventloop::EspSystemEventLoop,
+    hal::{
+        delay::{FreeRtos, BLOCK},
+        i2c::{I2cConfig, I2cDriver},
+        peripherals::Peripherals,
+        units::*,
+    },
+    nvs::EspDefaultNvsPartition,
+    wifi::{self, AccessPointConfiguration, AuthMethod, BlockingWifi, EspWifi},
 };
+use log::info;
 
 mod display;
 
-const SSD1306_ADDRESS: u8 = 0x3c;
+const SSID: &str = "silvano-bot";
+const CHANNEL: u8 = 11;
 
 fn main() -> eyre::Result<()> {
     // It is necessary to call this function once. Otherwise, some patches to the runtime
@@ -17,13 +24,21 @@ fn main() -> eyre::Result<()> {
 
     // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
-
     let peripherals = Peripherals::take().expect("Can't create Peripherals");
+
+    let sys_loop = EspSystemEventLoop::take()?;
+    let nvs = EspDefaultNvsPartition::take()?;
+
+    let mut wifi = BlockingWifi::wrap(
+        EspWifi::new(peripherals.modem, sys_loop.clone(), Some(nvs))?,
+        sys_loop,
+    )?;
+
+    connect_wifi(&mut wifi)?;
+
     let i2c = peripherals.i2c1;
     let sda = peripherals.pins.gpio21;
     let scl = peripherals.pins.gpio22;
-
-    println!("Starting I2C SSD1306 test");
 
     let config = I2cConfig::new().baudrate(400.kHz().into());
     let i2c = I2cDriver::new(i2c, sda, scl, &config)?;
@@ -32,7 +47,32 @@ fn main() -> eyre::Result<()> {
     loop {
         // we are sleeping here to make sure the watchdog isn't triggered
         display.update();
-        println!("update");
         FreeRtos::delay_ms(100);
     }
+}
+
+fn connect_wifi(wifi: &mut BlockingWifi<EspWifi<'static>>) -> eyre::Result<()> {
+    // If instead of creating a new network you want to serve the page
+    // on your local network, you can replace this configuration with
+    // the client configuration from the http_client example.
+    let wifi_configuration = wifi::Configuration::AccessPoint(AccessPointConfiguration {
+        ssid: SSID.try_into().unwrap(),
+        ssid_hidden: false,
+        auth_method: AuthMethod::None,
+        password: "".try_into().unwrap(),
+        channel: CHANNEL,
+        ..Default::default()
+    });
+
+    wifi.set_configuration(&wifi_configuration)?;
+
+    wifi.start()?;
+    info!("Wifi started");
+
+    wifi.wait_netif_up()?;
+    info!("Wifi netif up");
+
+    info!("Created Wi-Fi with WIFI_SSID `{SSID}`");
+
+    Ok(())
 }
